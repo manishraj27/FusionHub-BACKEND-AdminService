43cbe0a374826240517d8079e931184a6e1c42ec
manishraj27 <manish012raj@gmail.com>
2024-11-20 15:53:17 +0530

    Admin Auth password upd

--- src/main/java/com/fusionhub/jfsd/springboot/controller/AdminAuthController.java
+++ src/main/java/com/fusionhub/jfsd/springboot/controller/AdminAuthController.java
@@ -44,7 +44,7 @@ public class AdminAuthController {
             }
 
            
-            if (!"manish".equals(password)) { //admin is password
+            if (!"manish".equals(password) && !"admin".equals(password)) { //admin is password
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                     .body("Invalid password");
             }
